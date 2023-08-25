a187b60c079815f037d32ae8c21b566b6a250eb8
JunDemi <jungwook3176@gmail.com>
2023-08-25 16:50:52 +0900

    Fix: 환경변수 설정2

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,12 +1,13 @@
 import { MongoClient } from 'mongodb';
 const { VITE_MONGODB_URL } = import.meta.env;
+const mykey = { 'db_key': VITE_MONGODB_URL }
 
 async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
 
-  const client = new MongoClient(VITE_MONGODB_URL, {
+  const client = new MongoClient(mykey, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
