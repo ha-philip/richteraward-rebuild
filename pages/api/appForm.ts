175a6f672f4d6f043e2f664f95a7b0d97b288317
JunDemi <jungwook3176@gmail.com>
2023-08-29 16:13:05 +0900

    a

--- pages/api/appForm.ts
+++ pages/api/appForm.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+/*import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
@@ -47,3 +47,4 @@ async function addApp(req: NextApiRequest, res: NextApiResponse) {
     res.status(500).json({ error: "Error add notice", success: false });
   }
 }
+*/
